81aa49d1343d3885c582cd679078af5fc179e43c
Set Kyar Wa Lar <setkyar16@gmail.com>
2018-11-23 01:45:08 +0800

    add google analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="weMZD9p0nbzv5I-Dj3-tah8-LLaaAxPwMh-sHyJ3R5g" />
 
         <title>Laravel</title>
 
@@ -92,5 +93,15 @@
                 </div>
             </div>
         </div>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129646859-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-129646859-1');
+        </script>
     </body>
 </html>
